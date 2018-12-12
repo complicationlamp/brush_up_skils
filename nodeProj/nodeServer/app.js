ebe4a1a62ffe97fb9672959cf35eeb01643b788d
Kyle <lookerkj@protonmail.com>
2018-12-11 19:50:30 -0800

    messing with handelbars and ejs

--- nodeProj/nodeServer/app.js
+++ nodeProj/nodeServer/app.js
@@ -5,7 +5,10 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set('view engine', 'pug');
+
+app.set('view engine', 'ejs');
+//pug code
+// app.set('view engine', 'pug');
 app.set('views', 'views')
 
 const adminData = require('./routes/admin')
@@ -18,7 +21,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-	res.status(404).render('404', {pageTitle: 'Page not Found'})
+	res.status(404).render('404', {pageTitle: 'Page Not Found'})
 })
 
 app.listen(3000);
